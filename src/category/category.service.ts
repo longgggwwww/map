209fb598ebfae8256acb9fd73f4db5f8b40bf19f
DANH PHI LONG <longgggwww@gmail.com>
2024-01-09 09:50:49 +0700

    Filter find all categories

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -33,7 +33,7 @@ export class CategoryService {
       orderBy,
       include: {
         createdBy: true,
-        places: true,
+        // places: true,
         subCategory: true,
       },
     });
